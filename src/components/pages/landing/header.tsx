25cfdb63147fa7c87f5e91292bdcadb16a521809
Lucky-On-13 <victoirelucngami@gmail.com>
2025-06-02 11:33:28 +0200

    fix: build trouble fixed

--- src/components/pages/landing/header.tsx
+++ src/components/pages/landing/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from '@/components/ui/button';
-import { Menu, Search, X } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
 import Link from 'next/link';
 import React, { useState } from 'react'
 
